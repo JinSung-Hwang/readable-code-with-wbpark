b0ee13f47a0eb90e7b55606fd2eb39eb4e3147ef
JinSung-Hwang <wlstjd7782@gmail.com>
2025-12-13 03:47:15 +0900

    feature: 코드 다듬기 - 기능 유지보수하기1 - 리펙토링 진행

--- src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/EmptyCell.java
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/EmptyCell.java
@@ -47,7 +47,7 @@ public class EmptyCell implements Cell {
 
   @Override
   public boolean isChecked() {
-    return cellState.isChecked();
+    return cellState.isOpened();
   }
 
   @Override
--- src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/NumberCell.java
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/NumberCell.java
@@ -52,7 +52,7 @@ public class NumberCell implements Cell {
 
   @Override
   public boolean isChecked() {
-    return cellState.isChecked();
+    return cellState.isOpened();
   }
 
   @Override
